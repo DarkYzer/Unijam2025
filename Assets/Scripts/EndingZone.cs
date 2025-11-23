ba14cbc5b4fa216bb3f81152c010adfc9d492b90
DarkYzer <darkyzer337@gmail.com>
2025-11-23 09:34:58 +0100

    HAS WIN

--- Assets/Scripts/EndingZone.cs
+++ Assets/Scripts/EndingZone.cs
@@ -4,11 +4,33 @@ public class EndingZone : MonoBehaviour
 {
     public static int partsAmount = 0;
     public static int partsCompleted = 0;
+    public float offset;
+    public GameObject player;
+
+    bool HasWin()
+    {
+        Transform child; bool temp; Transform playerChild;
+        for ( int i = 0; i < transform.childCount; i++)
+        {
+            child = transform.GetChild(i);
+            temp = false;
+            for (int j = 0; j < player.transform.childCount; j++)
+            {
+                playerChild = player.transform.GetChild(j);
+                if (Vector2.Distance(child.position, playerChild.position) < offset)
+                    temp = true;
+            }
+            if (temp == false)
+                return false;
+        }
+        return true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(partsAmount <= partsCompleted && PlayerMovement.playerAmount == partsAmount) Debug.Log("gagné!");
-        
+        // if(partsAmount <= partsCompleted && PlayerMovement.playerAmount == partsAmount) Debug.Log("gagné!");
+        if (HasWin())
+            Debug.Log("gagné !!!!!!!!");
     }
 }
